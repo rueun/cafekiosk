f661e55a40b6be2cc57f830b4f11051ab53d3255
eunjeong <dkfah1229@naver.com>
2023-10-30 23:07:27 +0900

    #features Persistence Layer - 음료 출력 api 추가

---
+++ src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.spring.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingType;
+import sample.cafekiosk.spring.domain.product.response.ProductResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts() {
+
+        final List<Product> products = productRepository.findAllBySellingTypeIn(ProductSellingType.forDisplay());
+
+        return products.stream()
+            .map(ProductResponse::of)
+            .collect(Collectors.toList());
+
+    }
+}

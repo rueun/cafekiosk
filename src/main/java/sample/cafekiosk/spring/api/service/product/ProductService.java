02a2091168859d99eb6656800181f33430338b71
eunjeong <dkfah1229@naver.com>
2023-11-08 23:33:47 +0900

    #features 프로덕트 추가 api

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
@@ -15,6 +16,32 @@ import java.util.stream.Collectors;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    // 동시성 이슈
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        return ProductResponse.builder()
+            .productNumber(nextProductNumber)
+            .type(request.getType())
+            .sellingStatus(request.getSellingStatus())
+            .name(request.getName())
+            .price(request.getPrice())
+            .build();
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if(latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+
+    }
+
     public List<ProductResponse> getSellingProducts() {
 
         final List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -24,4 +51,5 @@ public class ProductService {
             .collect(Collectors.toList());
 
     }
+
 }

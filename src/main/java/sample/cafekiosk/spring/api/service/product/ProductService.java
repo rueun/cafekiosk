3494fd452a3fa8e78da16e94acba96641e8f2c47
eunjeong <dkfah1229@naver.com>
2023-11-08 23:49:43 +0900

    #feat Product 생성 메서드에 @Transactional 추가

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -17,6 +18,7 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     // 동시성 이슈
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         String nextProductNumber = createNextProductNumber();
 

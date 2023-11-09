bed735b755b97a4dd96c898bb08298540cceda98
eunjeong <dkfah1229@naver.com>
2023-11-09 23:21:28 +0900

    #features validation 에러 Handler 작성

---
+++ src/main/java/sample/cafekiosk/spring/api/ApiControllerAdvice.java
@@ -0,0 +1,19 @@
+package sample.cafekiosk.spring.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+
+@ControllerAdvice
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> handleBindException(BindException e) {
+        return ApiResponse.of(
+            HttpStatus.BAD_REQUEST,
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+}
--- src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -14,13 +14,13 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 @NoArgsConstructor
 public class ProductCreateRequest {
 
-    @NotNull
+    @NotNull(message = "상품타입은 필수입니다.")
     private ProductType type;
-    @NotNull
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private ProductSellingStatus sellingStatus;
-    @NotBlank
+    @NotBlank(message = "상품명은 필수입니다.")
     private String name;
-    @Positive
+    @Positive(message = "상품 가격은 0보다 커야합니다.")
     private int price;
 
     @Builder

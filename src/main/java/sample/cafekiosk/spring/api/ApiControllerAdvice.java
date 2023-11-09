7767d3966600ac2e0162ddcb33b63f3aed56f116
eunjeong <dkfah1229@naver.com>
2023-11-09 23:26:16 +0900

    #fix validation 에러 Handler 수정 - HttpStatus 추가

--- src/main/java/sample/cafekiosk/spring/api/ApiControllerAdvice.java
+++ src/main/java/sample/cafekiosk/spring/api/ApiControllerAdvice.java
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @ControllerAdvice
 public class ApiControllerAdvice {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> handleBindException(BindException e) {
         return ApiResponse.of(

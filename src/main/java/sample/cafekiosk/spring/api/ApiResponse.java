50687388285c8aebde547196e20a4fe5bef0c6e4
eunjeong <dkfah1229@naver.com>
2023-11-09 23:11:18 +0900

    #features 공통 Response 클래스 생성

---
+++ src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
@@ -0,0 +1,25 @@
+package sample.cafekiosk.spring.api;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiResponse<T> {
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return new ApiResponse<>(status, status.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(HttpStatus.OK, HttpStatus.OK.name(), data);
+    }
+}
--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.domain.product.response.ProductResponse;
@@ -19,10 +20,9 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    private void createProduct(
+    private ApiResponse<ProductResponse> createProduct(
         @RequestBody @Valid ProductCreateRequest request) {
-        productService.createProduct(request);
-
+        return ApiResponse.ok(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")

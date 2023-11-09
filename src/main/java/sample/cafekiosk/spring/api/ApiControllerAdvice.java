8871471b979894007814ecc06c18d4cb62b05a3b
eunjeong <dkfah1229@naver.com>
2023-11-09 23:34:09 +0900

    #fix ApiControllerAdvice, ApiResponse 파일 수정

--- src/main/java/sample/cafekiosk/spring/api/ApiControllerAdvice.java
+++ src/main/java/sample/cafekiosk/spring/api/ApiControllerAdvice.java
@@ -2,12 +2,11 @@ package sample.cafekiosk.spring.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -15,6 +14,7 @@ public class ApiControllerAdvice {
     public ApiResponse<Object> handleBindException(BindException e) {
         return ApiResponse.of(
             HttpStatus.BAD_REQUEST,
-            e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+            null);
     }
 }

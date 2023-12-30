98189ac4c8a73de69372ecdc83ce686fa83f44ac
은정 <dkfah1222@naver.com>
2023-12-30 15:05:07 +0900

    #features 테스트 커밋

--- src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
+++ src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class MailSendClient {
     public boolean sendEmail(String fromEmail, String toEmail, String subject, String content) {
         log.info("메일 전송");
+        log.info("fromEmail: " + fromEmail);
         throw new IllegalArgumentException("메일 전송에 실패했습니다.");
     }
 }
